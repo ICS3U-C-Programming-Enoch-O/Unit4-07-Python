702189cb4e3d554496d73816bee5fa2a24e71a96
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-05-05 16:54:52 +0000

    “five_ints_line”

---
+++ five_ints_line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created by: Enoch O
+# Created on: May 5, 2025
+
+
+def main():
+    for number in range(1000, 2001):
+        print(number, end=" ")
+        if number % 5 == 4:
+            print()
+
+
+main()
